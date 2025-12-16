057cb4e5994f60efee0672cc34a396afd2cc0d5f
andrey <andrey-wz@yandex.ru>
2025-12-16 17:38:19 +0500

    migration

---
+++ database/migrations/2025_12_16_104933_add_column_to_vk_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('vk_users', function (Blueprint $table) {
+            $table->string('name')->after('vk_id')->nullable();
+            $table->string('active')->default(1);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('vk_users', function (Blueprint $table) {
+            $table->dropColumn('name');
+            $table->dropColumn('active');
+        });
+    }
+};
